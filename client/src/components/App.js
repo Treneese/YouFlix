612245aaad712437b14224a7491a3d0b8bb184cc
Ken Richlin <krichlin@gmail.com>
2024-06-12 16:22:04 -0400

    template applied

---
+++ client/src/components/App.js
@@ -0,0 +1,8 @@
+import React, { useEffect, useState } from "react";
+import { Switch, Route } from "react-router-dom";
+
+function App() {
+  return <h1>Project Client</h1>;
+}
+
+export default App;
---
+++ client/src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import App from "./components/App";
+import "./index.css";
+import { createRoot } from "react-dom/client";
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(<App />);
