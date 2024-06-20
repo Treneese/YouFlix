4386d0b1c98e3b79ba1a8ee8e5431872e0248796
Ken Richlin <krichlin@gmail.com>
2024-06-19 20:30:24 -0400

    user login methods applied

--- server/models.py
+++ server/models.py
@@ -2,9 +2,9 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db, bcrypt
 
-# from app import bcrypt
+# from config import bcrypt
 
 # Models go here!
 
@@ -15,20 +15,20 @@ class User(db.Model, SerializerMixin):
     username = db.Column(db.String, unique = True)
     _password_hash = db.Column(db.String, nullable = False)
 
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+
+    @password_hash.setter
+    def password_hash(self, password):
+        # utf-8 encoding is required in python 3
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+
     def __repr__(self):
         return f'User {self.username}, ID {self.id}'
-
-    # @hybrid_property
-    # def password_hash(self):
-    #     return self._password_hash
-
-    # @password_hash.setter
-    # def password_hash(self, password):
-    #     # utf-8 encoding is required in python 3
-    #     password_hash = bcrypt.generate_password_hash(
-    #         password.encode('utf-8'))
-    #     self._password_hash = password_hash.decode('utf-8')
-
-    # def authenticate(self, password):
-    #     return bcrypt.check_password_hash(
-    #         self._password_hash, password.encode('utf-8'))
