a41ac1a9082815aed1a82c7ef8502825953c14d2
Ken Richlin <krichlin@gmail.com>
2024-06-20 13:26:12 -0400

    seed file now populates users and pw hash

--- server/models.py
+++ server/models.py
@@ -1,16 +1,14 @@
+from sqlalchemy.orm import validates
 from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
 from config import db, bcrypt
 
-# from config import bcrypt
-
 # Models go here!
 
 class User(db.Model, SerializerMixin):
     __tablename__ = 'users'
-
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String, unique = True)
     _password_hash = db.Column(db.String, nullable = False)
@@ -29,6 +27,20 @@ class User(db.Model, SerializerMixin):
     def authenticate(self, password):
         return bcrypt.check_password_hash(
             self._password_hash, password.encode('utf-8'))
+
+    @classmethod
+    def create_table(cls):
+        """ Create new table for users """
+        # SQL Here?
+        # CURSOR.execute(sql)
+        # CONN.commit()
+
+    @classmethod
+    def drop_table(cls):
+        """ Drop the table for Trains """
+        # SQL Here?
+        # CURSOR.execute(sql)
+        # CONN.commit()
 
     def __repr__(self):
         return f'User {self.username}, ID {self.id}'
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,43 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
+
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        print("Resetting Database")
+        # User.drop_table()
+        # User.create_table()
+
+        User.query.delete()
+
+        print("Generating test users")
+        # Populate the tables with some test data
+
+        users = []
+        usernames = []
+
+        # verify that usernames are unique
+
+        for i in range(10):
+            username = fake.email()
+            while username in usernames:
+                username = fake.email()
+            usernames.append(username)
+
+            user = User(
+                username=username,
+                # image_url=fake.url(),
+            )
+
+            user.password_hash = user.username + 'password'
+            users.append(user)
+
+        db.session.add_all(users)
+        db.session.commit()
+        print("Complete.")
